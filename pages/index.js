faed6ed6694a1f6686f5d5b632f290f6a42dd755
placeparks <86215469+placeparks@users.noreply.github.com>
2023-02-10 21:49:22 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,16 @@ export default function Component() {
   return (
 
     <div>
- <a class="home" href="https://sign-in-b4xz.vercel.app/">Home</a><hr/>
+ <a className="home" href="https://sign-in-b4xz.vercel.app/">Home</a><hr/>
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <button class="btn">
-  <svg class="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+      <button className="btn">
+  <svg className="arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span>Upload to Ipfs</span>
 </button>
      
     </div>
-    <div class="para">
+    <div className="para">
           
            
            <h1> IPFS CID : {uris}</h1>
@@ -42,8 +42,8 @@ export default function Component() {
               See current user file
             </a>
         </div>
-            <div class="imp">if file does not open remove (:/) after ipfs in browser's address bar <span> &#8594;</span> 'https://gateway.ipfscdn.io/ipfs/QmX....'  </div>
-            <div class="footer"><a target="_blank" href="https://bio.link/099" rel="noreferrer"><Image src="/mail.png" height={22} width={20} alt="contact"/></a>
+            <div className="imp">if file does not open remove (:/) after ipfs in browser's address bar <span> &#8594;</span> 'https://gateway.ipfscdn.io/ipfs/QmX....'  </div>
+            <div className="footer"><a target="_blank" href="https://bio.link/099" rel="noreferrer"><Image src="/mail.png" height={22} width={20} alt="contact"/></a>
 Developed by Mirac.eth</div>
     </div>
     
